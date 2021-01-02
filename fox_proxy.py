77fce459ec624bb6e7fb21413f3c860303a706d7
sean <stereo720130@gmial.com>
2021-01-02 10:02:58 +0800

    a

---
+++ fox_proxy.py
@@ -0,0 +1,6 @@
+import os
+os.environ['http_proxy'] = "http://account:password@10.191.131.15:3128"
+os.environ['https_proxy'] = "http://account:password@10.191.131.15:3128"
+#test
+#import requests
+#requests.get("http://www.google.com.tw")
